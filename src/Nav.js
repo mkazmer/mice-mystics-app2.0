43f558b2a4d1b01966a78d5368609639e7a4bf51
mkazmer <webbasser055@gmail.com>
2020-01-05 18:55:27 -0500

    more work on nav

--- src/Nav.js
+++ src/Nav.js
@@ -9,7 +9,12 @@ const Nav = () => {
   return (
     <div className="Nav">
       <div className="nav-bar">
-        <Link to={{ pathname: '/app' }}>
+        <Link
+          onClick={() => {
+            toggle(false)
+          }}
+          to={{ pathname: '/app' }}
+        >
           <img className="mouse-icon" alt="Mouse Icon" src={'./images/mouseIcon.png'} />
         </Link>
       </div>
@@ -21,10 +26,31 @@ const Nav = () => {
       >
         <div className="hamburger" />
       </button>
-      <div className={`nav-menu`}>
-        <Link to={{ pathname: '/app' }}>App</Link>
-        <Link to={{ pathname: '/campaigns' }}>Campaigns</Link>
-        <Link to={{ pathname: '/' }}>Home</Link>
+      <div className={`nav-menu ${open ? 'open' : ''}`}>
+        <Link
+          onClick={() => {
+            toggle(false)
+          }}
+          to={{ pathname: '/app' }}
+        >
+          App
+        </Link>
+        <Link
+          onClick={() => {
+            toggle(false)
+          }}
+          to={{ pathname: '/campaigns' }}
+        >
+          Campaigns
+        </Link>
+        <Link
+          onClick={() => {
+            toggle(false)
+          }}
+          to={{ pathname: '/' }}
+        >
+          Home
+        </Link>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style/index.scss'
+import './style/core.scss'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))

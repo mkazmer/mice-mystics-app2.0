624b94938a66d36602648bdd9ab70a8380e9e221
mkazmer <webbasser055@gmail.com>
2020-03-09 13:01:04 -0400

    starting about page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,32 @@ import React from 'react'
 import './Home.scss'
 
 const Home = () => {
-  return <div className="Home">This is the home page</div>
+  return (
+    <div className="Home">
+      <div>
+        <h1>{'Mice & Mystics App'}</h1>
+        <h4>Created by Mike Kazmer</h4>
+        <p>
+          I created this application as a way to calcuate minion and boss rolls quickly and easily.
+        </p>
+      </div>
+      <div>
+        <h2>Dashboard</h2>
+        <figure>
+          <div>{'1 - \t 2 -'}</div>
+          <img
+            src="./images/about/creature_card_example.png"
+            alt="Screenshot of Spider Creature Card"
+          />
+          <figcaption>Spider Minion Creature Card</figcaption>
+        </figure>
+      </div>
+      <div>
+        <h2>Campaigns</h2>
+        <p>Under Construction....</p>
+      </div>
+    </div>
+  )
 }
 
 export default Home

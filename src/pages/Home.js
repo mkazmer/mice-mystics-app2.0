7fc2c608924636a1f39dd079a8fec19a1cb2cb6a
mkazmer <webbasser055@gmail.com>
2020-03-09 17:11:15 -0400

    more about page stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,12 +15,46 @@ const Home = () => {
       <div>
         <h2>Dashboard</h2>
         <figure>
-          <div>{'1 - \t 2 -'}</div>
+          <div>
+            <ul>
+              <li>
+                <h5>1 - Creature Name</h5> For minions this will be the creature type, while bosses
+                will have their individual name.
+              </li>
+              <li>
+                <h5>2 - Remove Creature</h5> Click this button to remove that individual creature
+                card after they have been defeated.
+              </li>
+              <li>
+                <h5>3 - Health Markers</h5> These markers will indicate the number of hits required
+                to defeat them. To easily keep track of hits to each creature, you can click on the
+                heart to toggle it between Red and Grey.
+              </li>
+              <li>
+                <h5>4 - Inidividual Minion Count</h5>
+                {
+                  '(Minions only) will have a unique number to differentiate them from other minions of the same type.'
+                }
+              </li>
+              <li>
+                <h5>5 - Creature's Abilities</h5>
+              </li>
+              <li>
+                <h5>6 - Movement Roll Result</h5>
+              </li>
+              <li>
+                <h5>7 - Attack Roll Result</h5>
+              </li>
+              <li>
+                <h5>8 - Defense Roll Result</h5>
+              </li>
+            </ul>
+          </div>
           <img
             src="./images/about/creature_card_example.png"
             alt="Screenshot of Spider Creature Card"
           />
-          <figcaption>Spider Minion Creature Card</figcaption>
+          <figcaption>Minion Creature Card - Spider </figcaption>
         </figure>
       </div>
       <div>
